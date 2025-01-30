35127efc07d42b5ceeab30a091a217255f2954f7
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2025-01-30 19:15:48 +0000

    あ

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ with tab3:
     }
 
     # 平文入力
-    raw_text = st.text_input("暗号化したい文字列を入力")
+    raw_text = st.text_input("暗号化したい文字列を入力（入力後、Enterを押してください）")
 
     if str(raw_text) == "":
         st.error("【エラー】暗号化したい文字列を入力してください。")
